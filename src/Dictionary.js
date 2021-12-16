e1405ec38add94eff63c6b3ceacd38ca2d658972
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2021-12-16 16:49:41 +0000

    created results component to deals with api response

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+
+    setResults(response.data[0]);
   }
+
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -24,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
