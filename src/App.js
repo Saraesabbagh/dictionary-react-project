48aa18d68da5241f30a8f9e37d11362fff14814e
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2021-12-17 14:57:53 +0000

    cleaned up Css

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ export default function App() {
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary</h1>
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">Coded by Sara sabbagh</footer>
       </div>
